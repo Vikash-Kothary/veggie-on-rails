421c9b1de0f0f069535a55115a8e9a908e9440d5
Vikash Kothary <kothary.vikash@gmail.com>
2018-09-19 14:37:27 +0200

    Add sampling.py

---
+++ veggie-on-rails/sampling.py
@@ -0,0 +1,20 @@
+#!/usr/bin/venv python3
+"""
+sampling.py - Convert a video into frames
+"""
+
+import ffmpeg
+
+
+def main():
+	"""
+	See: https://github.com/kkroening/ffmpeg-python
+	"""
+    stream = ffmpeg.input('input.mp4')
+    .output(stream, 'output.mp4')
+    .run(stream)
+
+    # Flip video horizontally: stream.hflip(stream)
+
+if __name__ == '__main__':
+    main()
