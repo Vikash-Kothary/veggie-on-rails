a2b4c93bd4a6834e4c18f00df58d86daae9dc46e
Vikash Kothary <kothary.vikash@gmail.com>
2018-09-20 10:52:51 +0200

    Display images using bootstrap theme

---
+++ veggie-on-rails/static/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+    $('[id^=carousel-selector-]').click( function(){
+        var id_selector = $(this).attr("id");
+        var id = id_selector.substr(id_selector.length -1);
+        id = parseInt(id);
+        $('.carousel').carousel(id - 1);
+    });
+    $('img').click(function(){
+	   $('.selected').removeClass('selected'); // removes the previous selected class
+	   $(this).addClass('selected'); // adds the class to the clicked image
+	});
+});
+
